0955626349f3016f7d77a3cd96fff2c265847b42
jeremykoperecz <kopereczjeremy@gmail.com>
2023-02-07 15:41:52 +0100

    resolution probleme like

--- routes/sauce.js
+++ routes/sauce.js
@@ -11,6 +11,6 @@ router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like',auth, stuffCtrl.sauceLike);
+//router.post('/:id/like', auth, stuffCtrl.sauceLike);
 
 module.exports = router;
