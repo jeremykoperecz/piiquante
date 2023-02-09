6093e91064ebd57c5df47a862fc1c5956c2f9a67
jeremykoperecz <kopereczjeremy@gmail.com>
2023-02-09 17:07:13 +0100

    mise en place securité

--- middleware/email-validator.js
+++ middleware/email-validator.js
@@ -6,8 +6,8 @@ module.exports = (req, res, next) => {
 	if (validatorEmails.isEmail(req.body.email)) {
 		next();
 	} else {
-        return res
-            .status(BAD_REQUEST).json({ error: 'cet le bordel ton email' })
-        console.log('caca email')
-	}
+        res.status(BAD_REQUEST).json({ error: 'cet le bordel ton email' });
+        console.log(' email non valide');
+                      
+    }   
 };
